b3e2d0ef67c31374613a18299bcf0218b8e7d56d
edagarli <lizhi@lydiabox.com>
2016-04-12 16:00:44 +0800

    fix bugs

---
+++ Day08DrawerDemo/index.ios.js
@@ -0,0 +1,106 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+
+
+'use strict';
+
+var React = require('react-native');
+var {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Navigator,
+    NavigatorIOS,
+    } = React;
+
+var TimerMixin = require('react-timer-mixin');
+
+var SplashScreen = require('./SplashScreen');
+var MainScreen = require('./MainScreen');
+
+var _navigator;
+
+var Day08DrawerDemo = React.createClass({
+  mixins: [TimerMixin],
+
+  getInitialState: function() {
+    return {
+      splashed: false,
+    };
+  },
+
+  componentDidMount: function() {
+    this.setTimeout(
+        () => {
+          this.setState({splashed: true});
+        },
+        2000
+    );
+  },
+
+  RouteMapper: function(route, navigationOperations, onComponentRef) {
+    _navigator = navigationOperations;
+    // return (
+    //   <View style={styles.container}>
+    //     <MainScreen navigator={navigationOperations}/>
+    //   </View>
+    // );
+    if (route.name === 'home') {
+      return (
+          <View style={styles.container}>
+            <MainScreen navigator={navigationOperations}/>
+          </View>
+      );
+    } else if (route.name === 'story') {
+      return (
+          <View style={styles.container}>
+            <StoryScreen
+                style={{flex: 1}}
+                navigator={navigationOperations}
+                story={route.story} />
+          </View>
+      );
+    }
+  },
+
+  render: function() {
+    if (this.state.splashed) {
+      var initialRoute = {name: 'home'};
+      return (
+        // <NavigatorIOS
+        //   style={styles.container}
+        //   initialRoute={{
+        //     title: '首页',
+        //     component: MainScreen,
+        //   }}
+        // />
+          <Navigator
+              style={styles.container}
+              initialRoute={initialRoute}
+              configureScene={() => Navigator.SceneConfigs.FadeAndroid}
+              renderScene={this.RouteMapper}
+          />
+      );
+      // return (
+      //   <View style={styles.container}>
+      //     <MainScreen />
+      //   </View>
+      // );
+    } else {
+      return (
+          <SplashScreen />
+      );
+    }
+  }
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+AppRegistry.registerComponent('Day08DrawerDemo', () => Day08DrawerDemo);
